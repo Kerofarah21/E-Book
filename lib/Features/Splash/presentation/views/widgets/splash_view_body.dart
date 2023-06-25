f44feeec17ff60528384f48fa65eb169c0d59015
Kerofarah21 <k.farah2124@gmail.com>
2023-06-25 19:41:12 +0300

    add custom app bar to home view

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,7 @@
 import 'package:bookly/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly/Features/home/presentation/views/home_view.dart';
-import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -63,10 +61,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void navigateToHome() {
     Future.delayed(
       const Duration(seconds: 2),
-      () => Get.to(
-        const HomeView(),
-        transition: Transition.fade,
-        duration: kTransitionDuration,
+      () => Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeView(),
+        ),
+        (route) => false,
       ),
     );
   }
