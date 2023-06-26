62ffe14a1e5c1a977fafd4c3b5de3a9fa3056504
Kerofarah21 <k.farah2124@gmail.com>
2023-06-26 18:28:59 +0300

    add featured books list view

--- lib/Features/home/presentation/views/widgets/featured_book_item.dart
+++ lib/Features/home/presentation/views/widgets/featured_book_item.dart
@@ -1,13 +1,15 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class BookItem extends StatelessWidget {
-  const BookItem({super.key});
+class FeaturedBookItem extends StatelessWidget {
+  const FeaturedBookItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
         child: Container(
---
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/home/presentation/views/widgets/featured_book_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => const FeaturedBookItem(),
+        itemCount: 5,
+      ),
+    );
+  }
+}
