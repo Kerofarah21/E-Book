3c107c1d6cd4baa107c4875629cd835308d02ff7
Kerofarah21 <k.farah2124@gmail.com>
2023-06-28 21:33:34 +0300

    refactor book details view body

---
+++ lib/Features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,56 @@
+import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * .2,
+          ),
+          child: const CustomBookItem(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          'Harry Potter and The Goblet of Fire',
+          style: Styles.textStyle30,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'J.K. Rawling',
+            style: Styles.textStyle18.copyWith(
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 18.0,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37.0,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/similar_books_list_view.dart';
-import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,77 +8,22 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
+    return const CustomScrollView(
+      physics: BouncingScrollPhysics(),
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Column(
             children: [
-              const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: width * .2,
-                ),
-                child: const CustomBookItem(),
-              ),
-              const SizedBox(
-                height: 43,
-              ),
-              const Text(
-                'Harry Potter and The Goblet of Fire',
-                style: Styles.textStyle30,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              Opacity(
-                opacity: .7,
-                child: Text(
-                  'J.K. Rawling',
-                  style: Styles.textStyle18.copyWith(
-                    fontWeight: FontWeight.w500,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 18.0,
-              ),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(
-                height: 37.0,
-              ),
-              const BooksAction(),
-              const Expanded(
+              CustomBookDetailsAppBar(),
+              BookDetailsSection(),
+              Expanded(
                 child: SizedBox(
                   height: 50.0,
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30.0,
-                  ),
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              const SimilarBooksListView(),
-              const SizedBox(
+              SimilarBooksSection(),
+              SizedBox(
                 height: 40.0,
               ),
             ],
